2c9b05ddb5f24759e46dc038238130b76606cf06
u6513115 <porames.aim@student.mahidol.edu>
2023-01-30 16:37:14 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,18 +1,21 @@
 #define N 6
 #include <stdio.h>
-
+#include <stdlib.h>
 #include "sorting.h"
 
-int main() {
+int main(int argc, char* argv[]) {
   int a[N]={3,2,6,7,3,1};
+  // int a[N]={1, 2, 3, 4, 5, 6};
   int i,j,new_number;
 
-   display(a,N);
-  bubbleSort(a,N); 
+
+  display(a, N);
+  bubbleSort(a, N); 
+  display(a, N);
+
    //insertion(a,N);
 
   // selectionSort(a,N);
-   display(a,N);
  return 0;
 }
 
