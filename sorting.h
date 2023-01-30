1aa3da4ab9f8be521b43c7594b543cb06a5ad24d
u6513115 <porames.aim@student.mahidol.edu>
2023-01-30 16:37:02 +0700

    Update sorting.h

--- sorting.h
+++ sorting.h
@@ -14,7 +14,7 @@ void selectionSort(int data[], int length)
 { 
 	int i, j, m, mi; 
 	
-     display(data,length);
+  display(data,length);
      
 	
 } 
@@ -26,8 +26,9 @@ void insertion(int a[],int n){
 
 }
 
-void swap(int *a,int *b){
- int temp;
+void swap(int *a,int *b)
+{
+  int temp
   temp=*a;
   *a=*b;
   *b=temp;
@@ -36,16 +37,34 @@ void swap(int *a,int *b){
 
 void bubbleSort(int a[],int n){
 
-int i,j;
-int sorted;
-// how may pair to compare?
-for(j=1;j<=n-1;j++){
- 
-  display(a,n);
- }
-  
+  int i,j;
+  // how may pair to compare?
+
+  for(i=0;i<n-1;i++)
+  {
+    int sorted = 1;
+    printf("Round %d\n", i+1);
+    for(j=0;j<n - i - 1;j++)
+    {
+      if(a[j]>a[j+1])
+      {
+        swap(&a[j],&a[j + 1]);
+        sorted = 0;
+      }
+      display(a, n);
+    }
+    printf("\n");
+    if (sorted) break;
+  }
 }
 
-
-
+int Isprime(int x)
+{
+  if (x == 0 || x == 1) return 0;
+  for (int i = 2; i < x; i++)
+  {
+    if (x % i == 0) return 0;
+  }
+  return 1;
+}
 
