3d0b28458ad0266a9e87bfb7648212259cb14a0b
u6513115 <porames.aim@student.mahidol.edu>
2023-02-02 19:53:17 +0700

    add det

--- main.c
+++ main.c
@@ -1,22 +1,34 @@
-#define N 6
 #include <stdio.h>
 #include <stdlib.h>
 #include "sorting.h"
 
 int main(int argc, char* argv[]) {
-  int a[N]={3,2,6,7,3,1};
-  // int a[N]={1, 2, 3, 4, 5, 6};
-  int i,j,new_number;
+  int i,j = 0,new_number, N, *a;
 
+  N = argc - 1;
+  a = (int*)malloc(sizeof(int) * N);
+  int* prime = (int*)malloc(sizeof(int) * N);
+  for (i = 0; i < N; i++)
+  {
+    a[i] = atoi(argv[i+1]);
+    if (Isprime(a[i]) == 1){
+      prime[j] = a[i];
+      j++;
+    }
+  }
+  N = j;
+  if (N == 0) printf("No prime\n");
+  else{
+    display(prime,N);
+    bubbleSort(prime,N); 
+    display(prime,N);
+  }
+  // display(a,N);
+  // bubbleSort(a,N); 
 
-  display(a, N);
-  bubbleSort(a, N); 
-  display(a, N);
-
-   //insertion(a,N);
+  // insertion(a,N);
 
   // selectionSort(a,N);
+
  return 0;
 }
-
-
