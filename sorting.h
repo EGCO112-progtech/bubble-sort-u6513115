4321517e2083a34fd7fac5a670cff2ccd3123760
u6513115 <porames.aim@student.mahidol.edu>
2023-02-02 19:56:17 +0700

    edit det

--- sorting.h
+++ sorting.h
@@ -1,22 +1,20 @@
 // selection sort function module in C
 
 void display(int a[],int n){
-  int i;
 
-  for(i=0;i<n;i++)
-  {
-    printf("%5d",a[i]);
-  }
-  printf("\n");
+    int i;
+
+    for(i=0;i<n;i++)
+      printf("%5d",a[i]);
+    printf("\n");
   
 }
 
 void selectionSort(int data[], int length) 
 { 
 	int i, j, m, mi; 
-	
   display(data,length);
-     
+
 	
 } 
 
@@ -27,9 +25,8 @@ void insertion(int a[],int n){
 
 }
 
-void swap(int *a,int *b)
-{
-  int temp;
+void swap(int *a,int *b){
+ int temp;
   temp=*a;
   *a=*b;
   *b=temp;
@@ -38,36 +35,29 @@ void swap(int *a,int *b)
 
 void bubbleSort(int a[],int n){
 
-  int i,j;
   // how may pair to compare?
-
-  for(i=0;i<n-1;i++)
+  for(int i = 0; i < n-1; i++)
   {
-    int sorted = 1;
-    printf("Round %d\n", i+1);
-    for(j=0;j<n - i - 1;j++)
+    int sorted;
+    for (int j = 0; j < n-i-1; j++)
     {
-      if(a[j]>a[j+1])
+      if (a[j] > a[j + 1])
       {
-        swap(&a[j],&a[j + 1]);
+        swap(&a[j], &a[j+1]);
         sorted = 1;
       }
       display(a, n);
     }
-    printf("\n");
-    if (sorted==0) break;
+    if (sorted == 0) break;
   }
 }
 
-int Isprime(int x)
+int Isprime(int x) 
 {
-  if (x == 0 || x == 1) 
-    return 0;
-  for (int i = 2; i < x; i++)
+  if (x == 1 || x == 0) return 0;
+  for(int i = 2; i < x; i++)
   {
-    if (x % i == 0) 
-      return 0;
+    if (x % i == 0) return 0;
   }
   return 1;
 }
-
