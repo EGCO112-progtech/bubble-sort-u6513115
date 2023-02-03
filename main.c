c8247eec4fd2949460949da7c8130fb3dd23cdc2
Replit user <>
2023-02-03 08:45:25 +0000

    pormung

--- main.c
+++ main.c
@@ -1,34 +1,35 @@
+#include "sorting.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "sorting.h"
 
-int main(int argc, char* argv[]) {
-  int i,j = 0,new_number, N, *a;
+int main(int argc, char *argv[]) {
+  int i, j = 0, new_number, N, *a;
 
   N = argc - 1;
-  a = (int*)malloc(sizeof(int) * N);
-  int* prime = (int*)malloc(sizeof(int) * N);
-  for (i = 0; i < N; i++)
-  {
-    a[i] = atoi(argv[i+1]);
-    if (Isprime(a[i]) == 1){
+  a = (int *)malloc(sizeof(int) * N);
+  int *prime = (int *)malloc(sizeof(int) * N);
+  for (i = 0; i < N; i++) {
+    a[i] = atoi(argv[i + 1]);
+    if (Isprime(a[i]) == 1) {
       prime[j] = a[i];
       j++;
     }
   }
-  N = j;
-  if (N == 0) printf("No prime\n");
-  else{
-    display(prime,N);
-    bubbleSort(prime,N); 
-    display(prime,N);
-  }
+  // N = j;
+  /*
+    if (N == 0)
+      printf("No prime\n");
+    else {
+      display(prime, N);
+      bubbleSort(prime, N);
+      display(prime, N);
+    }*/
   // display(a,N);
-  // bubbleSort(a,N); 
+  // bubbleSort(a,N);
 
-  // insertion(a,N);
+  insertionSort(a,N);
 
-  // selectionSort(a,N);
+  //selectionSort(a, N);
 
- return 0;
+  return 0;
 }
--- sorting.h
+++ sorting.h
@@ -1,63 +1,91 @@
 // selection sort function module in C
+#include <stdio.h>
+void display(int a[], int n) {
 
-void display(int a[],int n){
+  int i;
 
-    int i;
+  for (i = 0; i < n; i++)
+    printf("%5d", a[i]);
+  printf("\n");
+}
 
-    for(i=0;i<n;i++)
-      printf("%5d",a[i]);
+void insertionSort(int x[], int n) {
+
+  int j,i;
+
+  for(j=1;j<n;j++)
+    {
+      int insert = x[j];
+      for(i=j-1;i>=0;i--)
+      {
+        if(insert>x[i]) //nsert is max
+          break;
+        else
+          x[i+1]=x[i]; //position x[i] forward
+      }
+      x[i+1] = insert;
+      display(x,n);
+      printf("\n");
+    }
+
+}
+
+void swap(int *a, int *b) {
+  int temp;
+  temp = *a;
+  *a = *b;
+  *b = temp;
+}
+
+void selectionSort(int a[], int n) {
+  int i, j;
+  int m = a[0]; // dec m is min
+  int mi = 0;
+
+  for (j = 0; j < n - 1; j++) {
+    m = a[j];
+    mi = j;
+    printf("m = %d\n", m);
+    printf("mi = %d\n", mi);
+    for (i = j + 1; i < n; i++) {
+      if (m > a[i])  //find real min
+      //if (a[mi] > a[i])
+      {
+        m = a[i];
+        mi = i;
+      }
+    }
+    swap(&a[j], &a[mi]);
+    printf("a[j] = %d\n", a[j]);
+    printf("a[mi] = %d\n", a[mi]);
+    display(a, n);
     printf("\n");
-  
+  }
 }
 
-void selectionSort(int data[], int length) 
-{ 
-	int i, j, m, mi; 
-  display(data,length);
-
-	
-} 
-
-
-
-void insertion(int a[],int n){
-
-
-}
-
-void swap(int *a,int *b){
- int temp;
-  temp=*a;
-  *a=*b;
-  *b=temp;
-  
-}
-
-void bubbleSort(int a[],int n){
+void bubbleSort(int a[], int n) {
 
   // how may pair to compare?
-  for(int i = 0; i < n-1; i++)
-  {
+  for (int i = 0; i < n - 1; i++) {
     int sorted;
-    for (int j = 0; j < n-i-1; j++)
-    {
-      if (a[j] > a[j + 1])
-      {
-        swap(&a[j], &a[j+1]);
+    for (int j = 0; j < n - i - 1; j++) {
+      if (a[j] > a[j + 1]) {
+        swap(&a[j], &a[j + 1]);
         sorted = 1;
       }
       display(a, n);
     }
-    if (sorted == 0) break;
+    if (sorted == 0)
+      break;
   }
 }
 
-int Isprime(int x) 
-{
-  if (x == 1 || x == 0) return 0;
-  for(int i = 2; i < x; i++)
-  {
-    if (x % i == 0) return 0;
+int Isprime(int x) {
+  if (x == 1 || x == 0)
+    return 0;
+  for (int i = 2; i < x; i++) {
+    if (x % i == 0)
+      return 0;
   }
   return 1;
 }
